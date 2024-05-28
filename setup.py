3bc12106171429f4536f6a4ed5bd5deb6b58a235
bum6894 <bum6894@naver.com>
2024-05-28 22:03:08 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 # setup.py
+from glob import glob
+from os.path import basename, splitext
 from setuptools import setup, find_packages
 
 setup(
@@ -6,10 +8,14 @@ setup(
     version='0.1.0',
     description='A package for managing dates and times.',
     author='OSD7',
+    url="https://github.com/OSD7/datetime_manager",
     packages=find_packages(),
-    install_requires=[
-        'pytz'
+    install_requires= ["pytz"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    tests_require=['unittest'],
+    python_requires='>=3.6',
 )
 
